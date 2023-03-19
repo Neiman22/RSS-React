e9db35406adebbcb0da7afe4ff98feb9fdd5a221
Neiman22 <pushkarev_av@inbox.ru>
2023-03-19 21:12:29 +0300

    feat: add localStorage

--- react-components/src/components/SeacrhBar.tsx
+++ react-components/src/components/SeacrhBar.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/SearchBar.css'
 import {productsArray} from '../data/products'
 import ProductCard from './ProductCard'
 
 function SearchBar () {
-    const [searchText, setSearchText] = useState('');
+    const [searchText, setSearchText] = useState(localStorage.getItem('searchText') || '');
     const [products, setProducts] = useState(productsArray);
 
     function searchCards(event: React.ChangeEvent<HTMLInputElement>) {
@@ -14,9 +14,15 @@ function SearchBar () {
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const filteredProducts = productsArray.filter(product => product.title.toLowerCase().includes(searchText.toLowerCase()));
-        console.log(filteredProducts);
         setProducts(filteredProducts);
     }
+
+    useEffect(() => {
+        return () => {
+          localStorage.setItem('searchText', searchText);
+        };
+    }, [searchText]);
+
     return (
         <div className='search__container'>
             <div className='search__block'>
